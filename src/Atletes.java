29c464538e6ae71e1eef473b60fae9b49510ecb5
roger.miranda <roger.miranda@students.salle.url.edu>
2021-01-03 19:00:07 +0100

    Read JSON files

---
+++ src/Atletes.java
@@ -0,0 +1,23 @@
+public class Atletes {
+    private String name;
+    private String lastname;
+    private int age;
+    private String nation;
+    private float distance;
+    private float time;
+    private String type;
+
+    /* OVERRIDED FUNCTIONS */
+    @Override
+    public String toString() {
+        return "Atletes{" +
+                "name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                ", nation='" + nation + '\'' +
+                ", distance=" + distance +
+                ", time=" + time +
+                ", type='" + type + '\'' +
+                '}';
+    }
+}
