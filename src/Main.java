1fffb4d2420c6be275c1196df4a8282386264465
roger.miranda <roger.miranda@students.salle.url.edu>
2021-01-03 19:23:31 +0100

    Read JSON

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,11 @@
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.Scanner;
 
 public class Main {
-    private static String readFile(File f) throws FileNotFoundException {
-        StringBuilder sb = new StringBuilder();
-        Scanner s = new Scanner(f);
-
-        while (s.hasNext()) sb.append(s.nextLine());
-
-        return sb.toString();
-    }
 
     public static void main(String []args) {
         try {
-            Gson gson = new GsonBuilder()
-                    .setDateFormat("HH:mm")
-                    .create();
-
-            Club []clubs = gson.fromJson(Main.readFile(new File("files/clubs/datasetS.json")), Club[].class);
-
-            Race []races = gson.fromJson(Main.readFile(new File("files/races/datasetS.json")), Race[].class);
+            Club []clubs = FileController.loadFile("files/clubs/datasetS.json", Club[].class);
+            Race []races = FileController.loadFile("files/races/datasetS.json", Race[].class);
             System.out.println();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
