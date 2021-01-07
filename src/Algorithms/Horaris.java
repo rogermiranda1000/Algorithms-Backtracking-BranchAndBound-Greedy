f0eb7cdaf8483e5c94887dcbcd5c99dd14c4f65a
roger.miranda <roger.miranda@students.salle.url.edu>
2021-01-07 15:46:45 +0100

    Scheduling (Greedy)

---
+++ src/Algorithms/Horaris.java
@@ -0,0 +1,35 @@
+package Algorithms;
+
+import Dades.Race;
+
+import java.util.*;
+
+public class Horaris implements Runnable {
+    private final Race[] list;
+    private Race[] sorted;
+
+    public Horaris(Race []races) {
+        ArrayList<Race> list = new ArrayList<>(Arrays.asList(races));
+        this.list = list.stream().sorted().toArray(Race[]::new); // s'ordena per data final
+    }
+
+    public Race []getSorted() {
+        return this.sorted;
+    }
+
+    @Override
+    public void run() {
+        ArrayList<Race> result = new ArrayList<>();
+        Date last = null;
+
+        // agafem sempre i quan no es solapin
+        for (Race r : this.list) {
+            if (last == null || !r.getStart().before(last)) {
+                result.add(r);
+                last = r.getEnd();
+            }
+        }
+
+        this.sorted = result.toArray(Race[]::new);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,10 @@
 import Algorithms.Curses;
+import Algorithms.Horaris;
 import Dades.Club;
 import Dades.Race;
 
 import java.io.FileNotFoundException;
+import java.util.Arrays;
 
 public class Main {
 
@@ -10,11 +12,18 @@ public class Main {
         try {
             Club []clubs = FileController.loadFile("files/clubs/datasetS.json", Club[].class);
             Race []races = FileController.loadFile("files/races/datasetS.json", Race[].class);
-            Curses c = new Curses(10, 5000);
-            c.run();
-            System.out.println("[*] " + c.getBest());
+
+            Horaris h = new Horaris(races);
+            h.run();
+            races = h.getSorted();
+            System.out.println("Es poden organitzar " + races.length + " carreres.");
+            System.out.println(Arrays.toString(races));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+        Curses c = new Curses(10, 5000);
+        c.run();
+        System.out.println("[*] " + c.getBest());
     }
 }
